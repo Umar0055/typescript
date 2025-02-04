0acbc4397a14baa707f5206d962f4010c3efb40b
umar0055 <umarsohail648@gmail.com>
2025-02-04 15:54:37 +0500

    bill program

--- script.ts
+++ script.ts
@@ -1,23 +1,18 @@
-let userinput:number=324;
+let userinput:number=110;
 let unit:number=40;
 if(userinput<= 200){
     let bill:number=unit*userinput;
-    let tax:number=userinput*1.5
+    let tax:number=userinput*1.5/100;
     let totalbill:number=bill+tax
     console.log(`your bill is ${bill} and unit rate is ${unit} so your tax is ${tax} to calculate all this your total bill is ${totalbill}`);
-}else if(userinput<= 400&&userinput>= 201){
+}else if(userinput<= 500&&userinput>= 201){
     let bill:number=unit*userinput;
-    let tax:number=userinput*2.5;
+    let tax:number=userinput*5.5/100;
     let totalbill:number=bill+tax
     console.log(`your bill is ${bill} and unit rate is ${unit} so your tax is ${tax} to calculate all this your total bill is ${totalbill}`);
-}else if(userinput<= 600 &&userinput>= 401){
+}else {
     let bill:number=unit*userinput;
-    let tax:number=userinput*3.5
-    let totalbill:number=bill+tax
-    console.log(`your bill is ${bill} and unit rate is ${unit} so your tax is ${tax} to calculate all this your total bill is ${totalbill}`);
-}else if(userinput<= 800 &&userinput>= 601){
-    let bill:number=unit*userinput;
-    let tax:number=userinput*4.5
+    let tax:number=userinput*7.5/100;
     let totalbill:number=bill+tax
     console.log(`your bill is ${bill} and unit rate is ${unit} so your tax is ${tax} to calculate all this your total bill is ${totalbill}`);
 }
